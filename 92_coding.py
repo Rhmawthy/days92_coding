854b571b918c81ca12c16734810cbdb481674087
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2023-01-12 16:06:13 +0800

    Add files via upload

---
+++ 92_coding.py
@@ -0,0 +1,10 @@
+angka = int(input("masukkan angka :"))
+
+prima = "merupakan bilangan prima"
+
+if angka == 1 or angka == 0:
+	prima = "bukan bilangan prima"
+for i in range (2,angka):
+	if angka % i == 0 :
+		prima = "bukan bilangan prima"
+print (angka,prima)
